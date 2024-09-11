8e07069f8850f5ca93c1e17933b13bdea4b50eed
Beginneraboutlife116 <pythonforeye@gmail.com>
2024-09-12 02:01:28 +0800

    Update nav.ejs and main.js

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log("Hello world!");
+const currentBase = '/iRun/pages/';
+
+const currentRoot = window.location.pathname.replace(currentBase, '').split('/')[0];
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach((navLink) => {
+  if (navLink.getAttribute('href') === currentRoot) {
+    navLink.classList.add('text-warning-100');
+    navLink.classList.remove('text-secondary-300');
+    navLink.firstElementChild.classList.add('bg-warning-100');
+    navLink.firstElementChild.classList.remove('bg-secondary-300');
+  } else {
+    navLink.classList.remove('text-warning-100');
+    navLink.classList.add('text-secondary-300');
+    navLink.firstElementChild.classList.remove('bg-warning-100');
+    navLink.firstElementChild.classList.add('bg-secondary-300');
+  }
+
+  navLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    const to = navLink.getAttribute('href');
+    if (to === currentRoot) return;
+    window.location.href = `${currentBase}${to}/`;
+  });
+});
